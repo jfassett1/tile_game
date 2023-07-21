e2d1ba4fa7f47197a72ca182187cde8d24686590
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-07-21 13:42:15 -0400

    Update fifteen.js

--- fifteen.js
+++ fifteen.js
@@ -1,201 +1,149 @@
-(function(){
-    var state = 1;
-    var puzzle = document.getElementById("puzzle");
+// Get the "puzzle" div.
+const puzzle = document.getElementById("puzzle");
 
-    //displaying a solved version of the puzzle
-    solve();
+// Configure the "Shuffle" button.
+const shuffle = document.getElementById("shuffle");
+shuffle.addEventListener("click", function() {shuffle_puzzle()});
 
-    //listen for the clicking of the puzzle tile
-    puzzle.addEventListener("click", function(e){
-        if(state == 1){
-            //sliding animation
-            puzzle.className = 'animate';
-            //shifing our clicked cell
-            shiftCell(e.target);
-        }
-    });
+// Configure the "Reset" button.
+const reset = document.getElementById("reset");
+reset.addEventListener("click", function() {reset_puzzle()});
 
-    //listen for the click of the solve and shuffle button
-    document.getElementById("shuffle").addEventListener("click", shuffle);
-    document.getElementById("solve").addEventListener("click", solve);
+// Track the blank space with x and y positions.
+let blank_x;
+let blank_y;
+// Track whether the puzzle can be clicked using a boolean value.
+let clickable;
+// Track whether the puzzle is being shuffled using a boolean value.
+let shuffling;
+// Track the adjacent tiles as an array of ids.
+let adjacent_tiles;
 
-    //making the solve function 
-    //solves the puzzle if the player gets stuck and displays the solved puzzle
+// Show the puzzle.
+reset_puzzle();
 
-    function solve(){
-        //base-case
-        if(state == 0){
-            return;
-        }
+// Show the tiles in the initial (solved) state.
+function reset_puzzle() {
+	// Clear any existing tiles.
+	puzzle.innerHTML = "";
+	let n = 1;
+	// Use a nested loop to create 4 rows and 4 columns.
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			// Skip the 16th position.
+			if (n > 15) continue;
+			// Create a new div with the "tile" class.
+			const tile = document.createElement("div");
+			tile.classList = "tile";
+			// The tile's id is its position "x-y".
+			tile.id = j + "-" + i;
+			// The tile's name is its number.
+			tile.name = n;
+			// Make the tile clickable.
+			tile.onclick = function() {move_tile(tile)}
+			// Position the tile.
+			tile.style.left = (j * 100) + "px";
+			tile.style.top = (i * 100) + "px";
+			// Position the background image within the tile.
+			tile.style.backgroundPositionX = (j * -100) + "px";
+			tile.style.backgroundPositionY = (i * -100) + "px";
+			// Display the tile number.
+			tile.innerHTML = n++;
+			// Add the tile to the puzzle.
+			puzzle.append(tile);
+		}
+	}
+	// Set the initial position of the blank space to the bottom right corner.
+	blank_x = 3;
+	blank_y = 3;
+	// Make the puzzle not clickable (until the player shuffles).
+	clickable = false;
+}
 
-        puzzle.innerHTML = '';
+// Move the tile to the blank space.
+function move_tile(tile) {
+	if (!shuffling) {
+		// Do not move the tile if the puzzle is not clickable.
+		if (!clickable) return;
+		// Do not move the tile if it is not movable.
+		if (!tile.classList.contains("movable")) return;
+	}
+	// Move the tile.
+	tile.style.left = (blank_x * 100) + "px";
+	tile.style.top = (blank_y * 100) + "px";
+	// Swap the positions of the tile and blank space.
+	const temp = tile.id.split("-");
+	tile.id = blank_x + "-" + blank_y;
+	blank_x = parseInt(temp[0]);
+	blank_y = parseInt(temp[1]);
+	// If the blank space is in the bottom right corner, check whether the puzzle is solved.
+	if (!shuffling && blank_x == 3 && blank_y == 3) check_puzzle();
+	// Make the tiles adjacent to the blank space movable.
+	if (!shuffling) set_movable_tiles();
+}
 
-        var n = 1;
-        for(var i = 0; i <= 3; i++){
-            for(var j = 0; j <= 3; j++){
-                var cell = document.createElement('span');
-                cell.id = 'cell-' + i + '-' + j;
-                //moving the cells to the left a certain pixel
-                cell.style.left = (j * 80 + 1 * j + 1) + 'px';
-                //moving cells up
-                cell.style.top = (j * 80 + 1 * j + 1) + 'px';
+// Make the tiles adjacent to the blank space movable.
+function set_movable_tiles() {
+	// Make the previous adjacent tiles not movable.
+	for (const tile_id of adjacent_tiles) {
+		// Skip the blank space.
+		if (tile_id == blank_x + "-" + blank_y) continue;
+		const tile = document.getElementById(tile_id);
+		tile.classList = "tile";
+	}
+	// Get the ids of the current adjacent tiles.
+	get_adjacent_tiles();
+	// Make the current adjacent tiles movable.
+	for (const tile_id of adjacent_tiles) {
+		const tile = document.getElementById(tile_id);
+		tile.classList = "movable tile";
+	}
+}
 
-                if(n <= 15){
-                    cell.classList.add('number');
-                    cell.classList.add((i % 2 == 0 && j % 2 > 0 || i % 2 > 0 && j % 2 == 0) ? 'dark' : 'light');
-                    cell.innerHTML = (n++).toString();
-                }
-                else{
-                    cell.className = 'empty';
-                }
+// Return an array containing the ids of all tiles adjacent to the blank space.
+function get_adjacent_tiles() {
+	adjacent_tiles = [];
+	// If the adjacent position is in bounds, add it to the array.
+	if (blank_x > 0) adjacent_tiles.push((blank_x - 1) + "-" + blank_y);
+	if (blank_x < 3) adjacent_tiles.push((blank_x + 1) + "-" + blank_y);
+	if (blank_y > 0) adjacent_tiles.push(blank_x + "-" + (blank_y - 1));
+	if (blank_y < 3) adjacent_tiles.push(blank_x + "-" + (blank_y + 1));
+}
 
-                puzzle.appendChild(cell);
+// Shuffle the puzzle in a way that is solvable.
+function shuffle_puzzle() {
+	shuffling = true;
+	// Move a random tile N times.
+	const N = 1000;
+	for (let i = 0; i < N; i++) {
+		// Get the ids of all adjacent tiles.
+		get_adjacent_tiles();
+		// Move a random adjacent tile.
+		const random_index = Math.floor(Math.random() * adjacent_tiles.length);
+		const tile_id = adjacent_tiles[random_index];
+		const tile = document.getElementById(tile_id);
+		move_tile(tile);
+	}
+	shuffling = false;
+	// Make the puzzle clickable.
+	set_movable_tiles();
+	clickable = true;
+}
 
-            }
-        }
-    }
-
-    //shifting the cells to the empty space
-    function shiftCell(cell){
-        //checking if the cell has a number
-        if(cell.className != 'empty'){
-            //getting the empty cell next to the current cell
-            var emptyCell = getEmptyAdjacentCell(cell);
-
-            if(emptyCell){
-                var temp = {style: cell.style.cssText, id: cell.id};
-
-                //changing the id value
-                cell.style.cssText = emptyCell.style.cssText;
-                cell.id = emptyCell.id;
-                emptyCell.style.cssText = temp.style;
-                emptyCell.id = temp.id;
-
-                if(state == 1){
-                    //checking the order of the number
-                    setTimeout(checkOrder, 150);
-                }
-            }
-        }
-
-    }
-
-    //getting the sepcific cell by row and column
-    function getCell(row, col){
-        return document.getElementById('cell-' + row + '-' + col);
-    }
-
-    //getting the empty cell
-    function getEmptyCell(){
-        return puzzle.querySelector('.empty');
-    }
-
-    //getting the empty adjacent cell if there is one
-    function getEmptyAdjacentCell(cell){
-        //getting all adjacent cells
-        var adjacent = getAjacentCells(cell);
-
-        //looking for the empty cell
-        for(var i = 0; i < adjacent.length; i++){
-            //if we found the cell that is empty
-            if(adjacent[i].className == 'empty'){
-                //return that cell value
-                return adjacent[i];
-            }
-        }
-        //if we cant find the empty cell
-        return false;
-
-    }
-    //getting all possible adjacent cells
-    function getAjacentCells(cell){
-        var id = cell.id.split('-');
-
-        //getting the cell position's indexes
-        var row = parseInt(id[1]);
-        var col = parseInt(id[2]);
-
-        var adjacent = [];
-
-        //checking all possible cells adjacent to the current cell
-
-        if(row < 3){
-            adjacent.push(getCell(row + 1, col));
-        }
-        if(row > 0){
-            adjacent.push(getCell(row - 1, col));
-        }
-        if(col < 3){
-            adjacent.push(getCell(row, col + 1));
-        }
-        if(row > 0){
-            adjacent.push(getCell(row, col - 1));
-        }
-
-        return adjacent;
-    }
-
-    //checking if the order of the numbers is correct
-    //later needs to change to see if the image is made in the correct order
-
-    function checkOrder(){
-        //checking to see if the empty cell is in the right position
-
-        if(getCell(3,3).className != 'empty'){
-            return;
-        }
-        
-        var n = 1;
-        //going through all the cells to check to see if the numbers are in order
-
-        for(var i = 0; i <= 3; i++){
-            for(var j = 0; j<= 3; j++){
-                if(n <= 15 && getCell(i,j).innerHTML != n.toString()){
-                    return;
-                }
-                n++;
-            }
-        }
-        if(confirm('Yeah, you did it. \nWant to play again')){
-            shuffle();
-        }
-    }
-
-    //shuffleing all the puzzle pieces
-    function shuffle(){
-        //base-case
-        if(state == 0){
-            return;
-        }
-
-        puzzle.removeAttribute('class');
-        state = 0;
-
-        var prevCell;
-        var i = 1;
-        var interval = setinterval(function(){
-            if(i <= 100){
-                var adjacent = getAjacentCells(getEmptyCell());
-                if(prevCell){
-                    for(var j = adjacent.length - 1; j > 0; j--){
-                        if(adjacent[i].innerHTML == prevCell.innerHTML){
-                            adjacent.splice(j, 1);
-                        }
-                    }
-                }
-                prevCell = adjacent[rand(0, adjacent.length - 1)];
-                shiftCell(prevCell);
-                i++;
-            }
-            else{
-                clearInterval(interval);
-                state = 1;
-            }
-        },5);
-    }
-
-    function rand(from, to){
-        return Math.floor(Math.random() * (to - from  + 1)) + from;
-    }
-
-}());
+// Check whether the puzzle is solved.
+function check_puzzle() {
+	// Use a nested loop to check each position.
+	let n = 1
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			// Skip the 16th position.
+			if (n > 15) continue;
+			const tile = document.getElementById(j + "-" + i);
+			// If the tile does not have the correct number, stop checking.
+			if (tile.name != n++) return;
+		}
+	}
+	// If the check passed, the puzzle is solved.
+	clickable = false;
+	// Do something here.
+}
